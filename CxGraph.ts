6baebf3b5028cd6e7f021071a247c4409304e1ee
Claus Fjellø-Jensen <claus.fjello@gmail.com>
2021-08-01 10:11:57 +0200

    explicit hierarkey import version

--- CxGraph.ts
+++ CxGraph.ts
@@ -1,4 +1,4 @@
-import { HierarKey } from 'https://deno.land/x/hierarkey/mod.ts'
+import { HierarKey } from 'https://deno.land/x/hierarkey@v1.0/mod.ts'
 
 export class Node<T> {  
     public type: string = ''
